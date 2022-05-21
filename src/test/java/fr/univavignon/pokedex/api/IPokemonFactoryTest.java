cf5d5d27713fbb5df6b12e4c38fd5e2566d29627
Ayoub-Zennouh <ayoub.zennouh@alumni.univ-avignon.fr>
2022-05-21 04:37:07 +0200

    rapport

--- src/test/java/fr/univavignon/pokedex/api/IPokemonFactoryTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokemonFactoryTest.java
@@ -2,17 +2,14 @@ package fr.univavignon.pokedex.api;
 
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.when;
 
 import org.junit.Test;
-import org.mockito.Mockito;
 
 public class IPokemonFactoryTest {
 	
 	@Test
 	public void createPokemonTest() {
-		IPokemonFactory pokemonFactory = new PokemonFactory();
-		new RocketPokemonFactory();
+		IPokemonFactory pokemonFactory = new RocketPokemonFactory();
 		
 		Pokemon pokemon = pokemonFactory.createPokemon(0, 613, 64, 4000, 4);
 		assertEquals(0, pokemon.getIndex());
@@ -20,6 +17,10 @@ public class IPokemonFactoryTest {
 		assertEquals(64, pokemon.getHp());
 		assertEquals(4000, pokemon.getDust());
 		assertEquals(4, pokemon.getCandy());
+		assertEquals("Bulbizarre", pokemon.getName());
+		assertEquals(126, pokemon.getAttack());
+		assertEquals(126, pokemon.getDefense());
+		assertEquals(90, pokemon.getStamina());
 		
 	}
 
